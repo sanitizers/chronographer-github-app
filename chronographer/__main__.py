97982a3eae7f0d3c82dedc5806dfcedaf3c518a7
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-12-03 17:41:07 +0100

    Wire up gidgethub event dispatcher

--- chronographer/__main__.py
+++ chronographer/__main__.py
@@ -1,9 +1,14 @@
 """Cronicler robot watching all the news being recorded as change notes!."""
 
 import asyncio
+from http import HTTPStatus
+import os
 import sys
 
 from aiohttp import web
+from gidgethub import BadRequest
+from gidgethub.routing import Router
+from gidgethub.sansio import Event
 
 
 global aiohttp_server  # Needed to kill all pending tasks correctly
@@ -23,7 +28,21 @@ async def configure_app(server):
 
 async def route_http_events(request):
     """Dispatch incoming webhook events to corresponsing handlers."""
-    return web.Response(text='OK')
+    if request.method != 'POST':
+        raise web.HTTPMethodNotAllowed(
+            method=request.method,
+            allowed_methods=('POST'),
+        ) from BadRequest(HTTPStatus.METHOD_NOT_ALLOWED)
+    router = Router()
+    secret = os.environ.get('GITHUB_WEBHOOK_SECRET')  # TODO: move to cfg layer
+    event = Event.from_http(
+        request.headers,
+        await request.read(),
+        secret=secret,
+    )
+    await asyncio.sleep(1)  # Give GitHub a sec to deal w/ eventual consistency
+    await router.dispatch(event)
+    return web.Response(text='OK: GitHub event received.')
 
 
 async def run_server(loop, host, port):
