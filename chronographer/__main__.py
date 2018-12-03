beeff1fc9bc32498155e2a6a0fc91b5a2d1f0a60
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-12-03 15:45:29 +0100

    Add a low-level server for routing events

--- chronographer/__main__.py
+++ chronographer/__main__.py
@@ -1,1 +1,17 @@
 """Cronicler robot watching all the news being recorded as change notes!."""
+
+from aiohttp import web
+
+
+async def build_server():
+    app = web.Server(route_http_events)
+    await configure_app(app)
+    return app
+
+
+async def configure_app(app):
+    pass
+
+
+async def route_http_events(request):
+    return web.Response(text='OK')
