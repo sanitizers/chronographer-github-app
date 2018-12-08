dad9a111dcc53c55259f5b9eac972ab39a7f59da
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-12-08 17:14:13 +0100

    Add docstrings to config classes

--- chronographer/config.py
+++ chronographer/config.py
@@ -8,20 +8,26 @@ envparse.Env.read_envfile()
 
 
 def get_config():
+    """Return an initialized config instance."""
     return environ.to_config(BotAppConfig)
 
 
 @environ.config
 class BotAppConfig:
+    """Bot app config."""
 
     @environ.config
     class GitHubAppIntegrationConfig:
+        """GitHub App auth related config."""
+
         app_id = environ.var(name='GITHUB_APP_IDENTIFIER')
         private_key = environ.var(name='GITHUB_PRIVATE_KEY')
         webhook_secret = environ.var(None, name='GITHUB_WEBHOOK_SECRET')
 
     @environ.config
     class WebServerConfig:
+        """Config of a web-server."""
+
         host = environ.var('0.0.0.0', name='HOST')
         port = environ.var(8080, name='PORT', converter=int)
 
