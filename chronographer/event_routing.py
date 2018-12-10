3fcba95bbb6b8904b7cd937b0db6f2329d76fd39
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-12-10 23:50:30 +0100

    Respond with HTTP 403 to quries with invalid sign

--- chronographer/event_routing.py
+++ chronographer/event_routing.py
@@ -5,7 +5,7 @@ from http import HTTPStatus
 import os
 
 from aiohttp import web
-from gidgethub import BadRequest
+from gidgethub import BadRequest, ValidationFailure
 from gidgethub.sansio import Event
 
 from .event_handlers import router
@@ -18,12 +18,17 @@ async def route_http_events(request):
             method=request.method,
             allowed_methods=('POST'),
         ) from BadRequest(HTTPStatus.METHOD_NOT_ALLOWED)
+
     secret = os.environ.get('GITHUB_WEBHOOK_SECRET')  # TODO: move to cfg layer
-    event = Event.from_http(
-        request.headers,
-        await request.read(),
-        secret=secret,
-    )
+    try:
+        event = Event.from_http(
+            request.headers,
+            await request.read(),
+            secret=secret,
+        )
+    except ValidationFailure as no_signature_exc:
+        raise web.HTTPForbidden from no_signature_exc
+
     await asyncio.sleep(1)  # Give GitHub a sec to deal w/ eventual consistency
     await router.dispatch(event)
     return web.Response(text=f'OK: GitHub event received. It is {event!r}')
