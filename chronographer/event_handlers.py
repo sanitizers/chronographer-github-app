ff95ae27ea4a80b193726d6a4c7ca2ad512e68a0
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-12-12 01:40:55 +0100

    Add a dummy pull_request event handler

--- chronographer/event_handlers.py
+++ chronographer/event_handlers.py
@@ -1,10 +1,26 @@
 """Webhook event handlers."""
+from functools import wraps
 import sys
 
+import aiohttp
+import gidgethub.aiohttp
 from gidgethub.routing import Router
+
+from .utils import USER_AGENT
 
 
 router = Router()  # pylint: disable=invalid-name
+
+
+def listen_to_event_actions(event_name, actions):
+    """Subscribe to multiple events."""
+    def decorator(original_function):
+        def wrapper(*args, **kwargs):
+            return original_function(*args, **kwargs)
+        for action in actions:
+            wrapper = router.register(event_name, action=action)(wrapper)
+        return wraps(original_function)(wrapper)
+    return decorator
 
 
 @router.register('ping')
@@ -28,3 +44,24 @@ async def on_install(event, app_installation):
         file=sys.stderr,
     )
     print(f'installation={app_installation!r}', file=sys.stderr)
+
+
+@listen_to_event_actions(
+    'pull_request',
+    {
+        'labeled', 'unlabeled',
+        'opened', 'reopened',
+        'synchronize',
+    },
+)
+async def on_pr(event, app_installation):
+    """React to GitHub App pull request webhook event."""
+    async with aiohttp.ClientSession() as session:
+        gh_api = gidgethub.aiohttp.GitHubAPI(
+            session,
+            USER_AGENT,
+            oauth_token=app_installation['access'].token,
+        )
+        print(f'got pull_request event', file=sys.stderr)
+        print(f'event {event.event!r}', file=sys.stderr)
+        print(gh_api)
