532efca488719946083f3c3c08582993513b42ed
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-12-13 11:43:04 +0100

    Post check updates via Checks API

--- chronographer/event_handlers.py
+++ chronographer/event_handlers.py
@@ -1,9 +1,15 @@
 """Webhook event handlers."""
+from datetime import datetime
 from functools import wraps
 from io import StringIO
 import re
 import sys
 
+from check_in.github_api import DEFAULT_USER_AGENT as CHECK_IN_USER_AGENT
+from check_in.github_checks_requests import (
+    NewCheckRequest, UpdateCheckRequest,
+    to_gh_query,
+)
 from gidgethub.routing import Router
 from unidiff import PatchSet
 
@@ -64,16 +70,70 @@ async def on_install(event, app_installation):
 async def on_pr(event, app_installation):
     """React to GitHub App pull request webhook event."""
     diff_url = event.data['pull_request']['diff_url']
+    head_branch = event.data['pull_request']['head']['ref']
+    head_sha = event.data['pull_request']['head']['sha']
+    repo_slug = event.data['repository']['full_name']
+    check_runs_base_uri = f'/repos/{repo_slug}/check-runs'
     async with GitHubAPIClient() as gh_api:
+        gh_api.requester = (
+            f'{gh_api.requester} built with {CHECK_IN_USER_AGENT}'
+        )
+
+        resp = await gh_api.post(
+            check_runs_base_uri,
+            accept='application/vnd.github.antiope-preview+json',
+            data=to_gh_query(NewCheckRequest(
+                head_branch, head_sha,
+                name='Timeline protection',
+                started_at=f'{datetime.utcnow().isoformat()}Z',
+            )),
+            oauth_token=app_installation['access'].token,
+        )
+        check_suite_id = resp['check_suite']['id']
+        check_run_id = resp['id']
+        print(
+            f'Check suite ID is f{check_suite_id}\n'
+            f'Check run ID is {check_run_id}',
+            file=sys.stderr,
+        )
+        check_runs_updates_uri = f'{check_runs_base_uri}/{check_run_id:d}'
+
         diff_text = await gh_api.getitem(
             diff_url,
             oauth_token=app_installation['access'].token,
         )
         diff = PatchSet(StringIO(diff_text))
+
+        resp = await gh_api.patch(
+            check_runs_updates_uri,
+            accept='application/vnd.github.antiope-preview+json',
+            data=to_gh_query(UpdateCheckRequest(
+                status='in_progress',
+                conclusion='neutral',
+                completed_at=f'{datetime.utcnow().isoformat()}Z',
+            )),
+            oauth_token=app_installation['access'].token,
+        )
+
         news_fragments_added = any(
             f.is_added_file for f in diff
             if _NEWS_FRAGMENT_RE.search(f.path)
         )
+
+        resp = await gh_api.patch(
+            check_runs_updates_uri,
+            accept='application/vnd.github.antiope-preview+json',
+            data=to_gh_query(UpdateCheckRequest(
+                status='completed',
+                conclusion=(
+                    'success' if news_fragments_added
+                    else 'action_required'
+                ),
+                completed_at=f'{datetime.utcnow().isoformat()}Z',
+            )),
+            oauth_token=app_installation['access'].token,
+        )
+
         print(
             'News fragments are '
             f'{"present" if news_fragments_added else "absent"}',
