d94dc0e71ac38afdb5e1a7581e58c9f005ff03f5
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-02-26 02:16:24 +0100

    🎨 Move app runner to octomachinery

--- chronographer/__main__.py
+++ chronographer/__main__.py
@@ -1,48 +1,8 @@
 """Cronicler robot runner."""
 
-import asyncio
-import logging
-import sys
-
-import attr
-
-from octomachinery.app.config import BotAppConfig
-from octomachinery.app.runtime.context import RUNTIME_CONTEXT
-from octomachinery.app.server.config import WebServerConfig
-from octomachinery.app.server.machinery import (
-    run_forever as run_server_forever,
-)
+from octomachinery.app.server.runner import run as run_app
 
 from . import event_handlers  # noqa: F401; pylint: disable=unused-import
-
-
-logger = logging.getLogger(__name__)
-
-
-def run_app():
-    """Start up a server using CLI args for host and port."""
-    config = BotAppConfig.from_dotenv()
-    if len(sys.argv) > 2:
-        config = attr.evolve(
-            config,
-            server=WebServerConfig(*sys.argv[1:3]),
-        )
-    RUNTIME_CONTEXT.config = config  # pylint: disable=assigning-non-slot
-    if config.runtime.debug:  # pylint: disable=no-member
-        logging.basicConfig(level=logging.DEBUG)
-
-        from octomachinery.github.config.utils import APP_VERSION
-        logger.debug(
-            ' App version: %s '.center(50, '='),
-            APP_VERSION,
-        )
-    else:
-        logging.basicConfig(level=logging.INFO)
-
-    try:
-        asyncio.run(run_server_forever(config))
-    except KeyboardInterrupt:
-        logger.info(' Exiting the app '.center(50, '='))
 
 
 __name__ == '__main__' and run_app()  # pylint: disable=expression-not-assigned
