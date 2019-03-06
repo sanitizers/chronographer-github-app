32b5c4ee7d533244c82086ebf65886426011cf4f
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-03-07 00:54:07 +0100

    🎨 Make User-Agent configurable

--- chronographer/__main__.py
+++ chronographer/__main__.py
@@ -1,8 +1,13 @@
 """Cronicler robot runner."""
 
 from octomachinery.app.server.runner import run as run_app
+from octomachinery.utils.versiontools import get_version_from_scm_tag
 
 from . import event_handlers  # noqa: F401; pylint: disable=unused-import
 
 
-__name__ == '__main__' and run_app()  # pylint: disable=expression-not-assigned
+__name__ == '__main__' and run_app(  # pylint: disable=expression-not-assigned
+    name='Chronographer-Bot',
+    version=get_version_from_scm_tag(root='..', relative_to=__file__),
+    url='https://github.com/apps/chronographer',
+)
