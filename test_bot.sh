46180147bfe4a1e5814a20785cfb57798b74da81
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-01-06 03:43:30 +0100

    Add security_advisory test event stub

--- test_bot.sh
+++ test_bot.sh
@@ -6,7 +6,7 @@ GITHUB_APP_WEBHOOKS_URL=http://localhost:8080
 >&2 echo ====================================================================
 >&2 echo
 
-for event in ping pull_request check_run
+for event in ping security_advisory pull_request check_run
 do
     GITHUB_EVENT_PATH="`pwd`/${event}_event.json"
     >&2 echo Testing "${event}"...
